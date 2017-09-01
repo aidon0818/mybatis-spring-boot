53768fccfc7e698ddcaa00b1e2cee7e3e61b1846
liudong <liudong@imaoding.com>
2017-09-01 18:30:04 +0800

    添加返回规范

--- src/main/java/tk/mybatis/springboot/controller/TestController.java
+++ src/main/java/tk/mybatis/springboot/controller/TestController.java
@@ -1,12 +1,16 @@
 package tk.mybatis.springboot.controller;
 
+import org.assertj.core.util.Maps;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.springboot.model.AjaxMessage;
 import tk.mybatis.springboot.model.Test;
 import tk.mybatis.springboot.service.TestService;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/test")
@@ -15,11 +19,13 @@ public class TestController {
     private TestService testService;
 
     @RequestMapping
-    public List<Test> getTestList() {
+    public AjaxMessage getTestList() {
+        Map<String, Object> processResult = new HashMap<String, Object>();
         Test test = new Test();
-        test.setId(1);
-//        test.setName("123");
+//        test.setId(1);
+        test.setName("123");
         List<Test> list = testService.getTestList(test);
-        return list;
+        processResult.put("test", list);
+        return AjaxMessage.succeed(processResult);
     }
 }
