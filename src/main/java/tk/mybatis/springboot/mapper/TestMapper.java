1013cc35857d660a5d948200ebc4d1b54ad3b7a7
liudong <liudong@imaoding.com>
2017-09-02 14:59:28 +0800

    继承分页插件

--- src/main/java/tk/mybatis/springboot/mapper/TestMapper.java
+++ src/main/java/tk/mybatis/springboot/mapper/TestMapper.java
@@ -1,9 +1,10 @@
 package tk.mybatis.springboot.mapper;
 
 import tk.mybatis.springboot.model.Test;
+import tk.mybatis.springboot.util.MyMapper;
 
 import java.util.List;
 
-public interface TestMapper {
+public interface TestMapper extends MyMapper<Test> {
     public List<Test> getTestList(Test test);
 }
