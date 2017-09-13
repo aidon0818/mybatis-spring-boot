9759c91c3db0b1a797684c24c95f76eaf5c29f1b
liudong <liudong@imaoding.com>
2017-09-13 16:53:16 +0800

    添加订单操作

---
+++ src/test/java/tk/mybatis/springboot/service/OrderMasterServiceTest.java
@@ -0,0 +1,46 @@
+package tk.mybatis.springboot.service;
+
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import tk.mybatis.springboot.model.OrderMaster;
+
+/**
+ * Created by Dong_Liu
+ * date：2017/9/13
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OrderMasterServiceTest {
+    @Autowired
+    private OrderMasterService orderMasterService;
+
+    @Test
+    public void getAll() throws Exception {
+    }
+
+    @Test
+    public void getById() throws Exception {
+    }
+
+    @Test
+    public void deleteById() throws Exception {
+    }
+
+    @Test
+    public void save() throws Exception {
+    }
+
+    @Test
+    public void findByBuyOpenId() throws Exception {
+        OrderMaster orderMaster = new OrderMaster();
+        orderMaster.setOrderId("1234567");
+        List<OrderMaster> orderMasterList = (List<OrderMaster>) orderMasterService.findByBuyOpenId(orderMaster);
+        Assert.assertNotEquals(0, orderMasterList.size());
+    }
+}
