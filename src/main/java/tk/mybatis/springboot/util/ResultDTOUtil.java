be493855dfaf45692522a1e7027a0cbe7bf83bcf
liudong <liudong@imaoding.com>
2017-09-13 15:09:26 +0800

    新增API接口 获取卖家商品

---
+++ src/main/java/tk/mybatis/springboot/util/ResultDTOUtil.java
@@ -0,0 +1,24 @@
+package tk.mybatis.springboot.util;
+
+import tk.mybatis.springboot.dto.ResultDTO;
+
+public class ResultDTOUtil {
+    public static ResultDTO success(Object object) {
+        ResultDTO resultDTO = new ResultDTO();
+        resultDTO.setData(object);
+        resultDTO.setCode(0);
+        resultDTO.setMsg("成功");
+        return resultDTO;
+    }
+
+    public static ResultDTO success() {
+        return success(null);
+    }
+
+    public static ResultDTO error(Integer code, String msg) {
+        ResultDTO resultDTO = new ResultDTO();
+        resultDTO.setCode(code);
+        resultDTO.setMsg(msg);
+        return resultDTO;
+    }
+}
