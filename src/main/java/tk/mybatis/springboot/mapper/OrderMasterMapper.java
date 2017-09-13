d5546e43681349ab942bf7d912f2184638c1118c
liudong <liudong@imaoding.com>
2017-09-13 16:51:50 +0800

    添加订单操作

--- src/main/java/tk/mybatis/springboot/mapper/OrderMasterMapper.java
+++ src/main/java/tk/mybatis/springboot/mapper/OrderMasterMapper.java
@@ -1,7 +1,15 @@
 package tk.mybatis.springboot.mapper;
 
+import java.util.List;
+
 import tk.mybatis.springboot.model.OrderMaster;
 import tk.mybatis.springboot.util.MyMapper;
 
+/**
+ * @Author dong_liu
+ * @Date 2017/9/13 15:34
+ * 订单
+ */
 public interface OrderMasterMapper extends MyMapper<OrderMaster> {
+    List<OrderMaster> findByBuyOpenId(OrderMaster orderMaster);
 }
