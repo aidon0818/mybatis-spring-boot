3cae0434fe9e865fca6193d8cf50cd82c01bf653
liudong <liudong@imaoding.com>
2017-09-14 16:42:20 +0800

    添加订单操作

---
+++ src/main/java/tk/mybatis/springboot/dto/CartDTO.java
@@ -0,0 +1,39 @@
+package tk.mybatis.springboot.dto;
+
+
+
+/**
+ * 购物车
+ * Created by 廖师兄
+ * 2017-06-11 19:37
+ */
+
+public class CartDTO {
+
+    /** 商品Id. */
+    private String productId;
+
+    /** 数量. */
+    private Integer productQuantity;
+
+    public CartDTO(String productId, Integer productQuantity) {
+        this.productId = productId;
+        this.productQuantity = productQuantity;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Integer getProductQuantity() {
+        return productQuantity;
+    }
+
+    public void setProductQuantity(Integer productQuantity) {
+        this.productQuantity = productQuantity;
+    }
+}
