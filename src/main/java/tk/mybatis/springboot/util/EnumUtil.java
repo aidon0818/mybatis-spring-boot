107c6968fdaee2197f7f4f0e1c3c14d33e8e7d04
liudong <liudong@imaoding.com>
2017-09-14 17:18:56 +0800

    添加订单操作

---
+++ src/main/java/tk/mybatis/springboot/util/EnumUtil.java
@@ -0,0 +1,16 @@
+package tk.mybatis.springboot.util;
+
+
+import tk.mybatis.springboot.enumUtil.CodeEnum;
+
+public class EnumUtil {
+
+    public static <T extends CodeEnum> T getByCode(Integer code, Class<T> enumClass) {
+        for (T each: enumClass.getEnumConstants()) {
+            if (code.equals(each.getCode())) {
+                return each;
+            }
+        }
+        return null;
+    }
+}
---
+++ src/main/java/tk/mybatis/springboot/util/Test001.java
@@ -0,0 +1,13 @@
+package tk.mybatis.springboot.util;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by Dong_Liu
+ * date：2017/9/14
+ */
+public class Test001 {
+    public static void main(String[] args) {
+
+    }
+}
