e151ef557e8d4a8542693028ac3776464f88b78a
aidon <30116992@qq.com>
2017-09-25 23:52:52 +0800

    Merge branch 'master' of /Users/Aidon/GitHub/mybatis-spring-boot with conflicts.

--- src/main/java/tk/mybatis/springboot/controller/TestController.java
+++ src/main/java/tk/mybatis/springboot/controller/TestController.java
@@ -1,9 +1,9 @@
 package tk.mybatis.springboot.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import tk.mybatis.springboot.model.AjaxMessage;
 import tk.mybatis.springboot.model.Test;
 import tk.mybatis.springboot.service.TestService;
 
@@ -13,14 +13,19 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/test")
-
 public class TestController {
-   @Value("${com.name}")
-   private String testName;
+    @Autowired
+    private TestService testService;
 
     @RequestMapping
-    public String getTestList() {
+    public AjaxMessage getTestList() {
+        Map<String, Object> processResult = new HashMap<String, Object>();
+        Test test = new Test();
+        test.setId(1);
+//        test.setName("123");
+        List<Test> list = testService.getTestList(test);
 
-        return testName;
+        processResult.put("test", list);
+        return AjaxMessage.succeed(processResult);
     }
 }
