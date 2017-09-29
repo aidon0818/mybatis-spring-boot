7629d883dad13861ba2b070204bddcedc94b367a
liudong <liudong@imaoding.com>
2017-09-29 17:58:59 +0800

    修改

--- src/main/java/tk/mybatis/springboot/service/impl/PushMessageServiceImpl.java
+++ src/main/java/tk/mybatis/springboot/service/impl/PushMessageServiceImpl.java
@@ -1,5 +1,6 @@
 package tk.mybatis.springboot.service.impl;
 
+import me.chanjar.weixin.mp.api.WxMpService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import tk.mybatis.springboot.dto.OrderDTO;
@@ -11,8 +12,9 @@ import tk.mybatis.springboot.service.PushMessageService;
  */
 @Service
 public class PushMessageServiceImpl implements PushMessageService {
-    //    @Autowired
-//    private WxMpService wxMpService;
+    @Autowired
+    private WxMpService wxMpService;
+
     @Override
     public void orderStatus(OrderDTO orderDTO1) {
 
