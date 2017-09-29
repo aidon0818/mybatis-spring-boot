b65ed2d3bdf9e520607cec03e2779aea78f95dee
liudong <liudong@imaoding.com>
2017-09-29 14:58:11 +0800

    卖家类目操作

---
+++ src/main/java/tk/mybatis/springboot/controller/SellerProductController.java
@@ -0,0 +1,157 @@
+package tk.mybatis.springboot.controller;
+
+import com.alibaba.druid.util.StringUtils;
+import com.github.pagehelper.PageInfo;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import tk.mybatis.springboot.exception.SellException;
+import tk.mybatis.springboot.form.ProductForm;
+import tk.mybatis.springboot.model.ProductCategory;
+import tk.mybatis.springboot.model.ProductInfo;
+import tk.mybatis.springboot.service.ProductCategoryService;
+import tk.mybatis.springboot.service.ProductService;
+import tk.mybatis.springboot.util.KeyUtil;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 卖家端商品
+ * Created by Dong_Liu
+ * date：2017/9/29
+ */
+@Controller
+@RequestMapping("/seller/product")
+public class SellerProductController {
+    @Autowired
+    private ProductService productService;
+
+    @Autowired
+    private ProductCategoryService productCategoryService;
+
+    /**
+     * 列表
+     *
+     * @param page
+     * @param size
+     * @param map
+     * @return
+     */
+    @GetMapping("/list")
+    public ModelAndView list(@RequestParam(value = "page", defaultValue = "1") Integer page,
+                             @RequestParam(value = "size", defaultValue = "10") Integer size,
+                             Map<String, Object> map) {
+        ProductInfo productInfo = new ProductInfo();
+        productInfo.setPage(page - 1);
+        productInfo.setRows(size);
+        List<ProductInfo> productInfoPage = productService.findAll(productInfo);
+        map.put("productInfoPage", new PageInfo<ProductInfo>(productInfoPage));
+        map.put("currentPage", page);
+        map.put("size", size);
+        return new ModelAndView("product/list", map);
+    }
+
+    /**
+     * 商品上架
+     *
+     * @param productId
+     * @param map
+     * @return
+     */
+    @RequestMapping("/on_sale")
+    public ModelAndView onSale(@RequestParam("productId") String productId,
+                               Map<String, Object> map) {
+        try {
+            productService.onSale(productId);
+        } catch (SellException e) {
+            map.put("msg", e.getMessage());
+            map.put("url", "/sell/seller/product/list");
+            return new ModelAndView("common/error", map);
+        }
+
+        map.put("url", "/sell/seller/product/list");
+        return new ModelAndView("common/success", map);
+    }
+
+    /**
+     * 商品下架
+     *
+     * @param productId
+     * @param map
+     * @return
+     */
+    @RequestMapping("/off_sale")
+    public ModelAndView offSale(@RequestParam("productId") String productId,
+                                Map<String, Object> map) {
+        try {
+            productService.offSale(productId);
+        } catch (SellException e) {
+            map.put("msg", e.getMessage());
+            map.put("url", "/sell/seller/product/list");
+            return new ModelAndView("common/error", map);
+        }
+
+        map.put("url", "/sell/seller/product/list");
+        return new ModelAndView("common/success", map);
+    }
+
+    @GetMapping("/index")
+    public ModelAndView index(@RequestParam(value = "productId", required = false) String productId,
+                              Map<String, Object> map) {
+        if (!StringUtils.isEmpty(productId)) {
+            ProductInfo productInfo = productService.findOne(productId);
+            map.put("productInfo", productInfo);
+        }
+
+        //查询所有的类目
+        ProductCategory productCategory = new ProductCategory();
+        List<ProductCategory> categoryList = productCategoryService.getAll(productCategory);
+        map.put("categoryList", categoryList);
+
+        return new ModelAndView("product/index", map);
+    }
+
+    /**
+     * 保存/更新
+     *
+     * @param form
+     * @param bindingResult
+     * @param map
+     * @return
+     */
+    @PostMapping("/save")
+    public ModelAndView save(@Valid ProductForm form,
+                             BindingResult bindingResult,
+                             Map<String, Object> map) {
+        if (bindingResult.hasErrors()) {
+            map.put("msg", bindingResult.getFieldError().getDefaultMessage());
+            map.put("url", "/sell/seller/product/index");
+            return new ModelAndView("common/error", map);
+        }
+        ProductInfo productInfo = new ProductInfo();
+        try {
+            //如果productId为空, 说明是新增
+            if (!StringUtils.isEmpty(form.getProductId())) {
+                productInfo = productService.findOne(form.getProductId());
+            } else {
+                form.setProductId(KeyUtil.genUniqueKey());
+            }
+            BeanUtils.copyProperties(form, productInfo);
+            productService.save(productInfo);
+        } catch (SellException e) {
+            map.put("msg", e.getMessage());
+            map.put("url", "/sell/seller/product/index");
+            return new ModelAndView("common/error", map);
+        }
+        map.put("url", "/sell/seller/product/list");
+        return new ModelAndView("common/success", map);
+    }
+}
