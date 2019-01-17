33fa14c2187005b44e8fb54e45842c1f940a0ae8
aidon0818 <30116992@qq.com>
2019-01-17 16:49:09 +0800

    设置异步方法

--- src/main/java/tk/mybatis/springboot/util/ThreadUtil.java
+++ src/main/java/tk/mybatis/springboot/util/ThreadUtil.java
@@ -1,5 +1,7 @@
 package tk.mybatis.springboot.util;
 
+import java.util.concurrent.TimeUnit;
+
 /**
  * @Auther: ld
  * @Date: 2019/1/15 15:02
@@ -7,14 +9,15 @@ package tk.mybatis.springboot.util;
  * @Description:
  */
 public class ThreadUtil {
-    public void sendMsg(final String msg){
+    public String sendMsg(final String msg){
         System.out.println("客户端：发送的消息为：" + msg);
         new Thread(new Runnable() {
             @Override
             public void run() {
                 try {
-                    Thread.sleep(1000);
-                    System.out.println("异步方法");
+                    Thread.sleep(5*1000);
+
+                    System.out.println("调用异步方法");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
                 }
@@ -22,10 +25,13 @@ public class ThreadUtil {
             }
         }).start();
         System.out.println("客户端：异步发送成功");
+        return msg;
     }
 
     public static void main(String[] args) {
         ThreadUtil threadUtil=new ThreadUtil();
-        threadUtil.sendMsg("msg");
+        String result=threadUtil.sendMsg("msg");
+        System.out.println(result);
+        System.out.println("主线程继续进行。。。");
     }
 }
