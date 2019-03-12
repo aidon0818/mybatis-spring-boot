28ac52100f55bb405e9fd66a28b8a1a93e448b4d
aidon0818 <30116992@qq.com>
2019-03-12 14:15:21 +0800

    随机彩票

--- src/main/java/tk/mybatis/springboot/util/caipiao.java
+++ src/main/java/tk/mybatis/springboot/util/caipiao.java
@@ -15,18 +15,13 @@ public class caipiao {
         while (true) {
             int sui = (int) (Math.random() * 33) + 1;// 获取33以内的数字
             set.add(sui < 10 ? "0" + sui : "" + sui);//将元素存入集合中
-            if (set.size() == 5) {//存满5个红色球，则结束循环
+            if (set.size() == 6) {//存满5个红色球，则结束循环
                 break;
             }
         }
         Set<String> set2 = new TreeSet<String>();
-        while (true) {
-            int sui2 = (int) (Math.random() * 16) + 1;// 获取16以内的数字
-            set2.add(sui2 < 10 ? "0" + sui2 : "" + sui2);
-            if (set2.size() == 2) {
-                break;
-            }
-        }
+        int sui2 = (int) (Math.random() * 16) + 1;// 获取16以内的数字
+        set2.add(sui2 < 10 ? "0" + sui2 : "" + sui2);
 
         System.out.println("红球：" + set + " 蓝球：" + set2);
 
