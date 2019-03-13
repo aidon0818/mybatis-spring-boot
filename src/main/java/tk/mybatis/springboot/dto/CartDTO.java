70ed1a5ac086ba7d0af3453cad4725b780fcd3f0
aidon0818 <30116992@qq.com>
2019-03-13 14:54:21 +0800

    优化代码

--- src/main/java/tk/mybatis/springboot/dto/CartDTO.java
+++ src/main/java/tk/mybatis/springboot/dto/CartDTO.java
@@ -1,6 +1,8 @@
 package tk.mybatis.springboot.dto;
 
 
+import java.math.BigDecimal;
+import java.util.Date;
 
 /**
  * 购物车
@@ -15,6 +17,10 @@ public class CartDTO {
 
     /** 数量. */
     private Integer productQuantity;
+
+    private Date createTime;
+
+    private BigDecimal amt;
 
     public CartDTO(String productId, Integer productQuantity) {
         this.productId = productId;
@@ -36,4 +42,49 @@ public class CartDTO {
     public void setProductQuantity(Integer productQuantity) {
         this.productQuantity = productQuantity;
     }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    public BigDecimal getAmt() {
+        return amt;
+    }
+
+    public void setAmt(BigDecimal amt) {
+        this.amt = amt;
+    }
+
+    @Override
+    public String toString() {
+        return "CartDTO{" +
+                "productId='" + productId + '\'' +
+                ", productQuantity=" + productQuantity +
+                ", createTime=" + createTime +
+                ", amt=" + amt +
+                '}';
+    }
+    @Override
+    public boolean equals(final Object obj) {
+        if (obj == null) {
+            return false;
+        }
+        final CartDTO book = (CartDTO) obj;
+        if (this == book) {
+            return true;
+        } else {
+            return (this.productId.equals(book.productId) && this.productQuantity == book.productQuantity);
+        }
+    }
+    @Override
+    public int hashCode() {
+        int hashno = 7;
+        hashno = 13 * hashno + (productId == null ? 0 : productId.hashCode());
+        return hashno;
+    }
+
 }
